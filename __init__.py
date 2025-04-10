e73bb2a6ae1207decae1e955d81abca7a9060a14
tekanoo <150429577+tekanoo@users.noreply.github.com>
2025-04-10 17:23:00 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,8 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
